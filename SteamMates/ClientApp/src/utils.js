dae77a6885b0b35d17740b742abca37eedb5df8c
David Schmidt <david.adam.schmidt@gmail.com>
2020-01-19 20:02:15 +0100

    Order search results by relevance

--- SteamMates/ClientApp/src/utils.js
+++ SteamMates/ClientApp/src/utils.js
@@ -1,16 +1,31 @@
 export const getMatchingFriends = (friends, searchTerm) => {
-  if (searchTerm.length === 0) {
-    return [];
-  }
-
   const matches = [];
 
-  for (let friend of friends) {
-    let startIndex = friend.personaName.toLowerCase().indexOf(searchTerm);
+  if (searchTerm.length === 0) {
+    return matches;
+  }
+
+  for (const friend of friends) {
+    const startIndex = friend.personaName.toLowerCase().indexOf(searchTerm);
     if (startIndex > -1) {
       matches.push({ friend: friend, startIndex: startIndex });
     }
   }
 
+  matches.sort(matchComparer);
+
   return matches;
+};
+
+const matchComparer = (a, b) => {
+  const startIndexDiff = a.startIndex - b.startIndex;
+
+  return startIndexDiff !== 0
+    ? startIndexDiff
+    : a.friend.personaName
+        .toLowerCase()
+        .substring(a.startIndex)
+        .localeCompare(
+          b.friend.personaName.toLowerCase().substring(b.startIndex)
+        );
 };
