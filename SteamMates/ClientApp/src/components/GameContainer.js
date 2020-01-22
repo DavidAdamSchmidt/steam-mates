604e37e8823a82c5823650e3791ca9a24af1c086
David Schmidt <david.adam.schmidt@gmail.com>
2020-01-23 00:14:42 +0100

    Send selected friend ids to server

--- SteamMates/ClientApp/src/components/GameContainer.js
+++ SteamMates/ClientApp/src/components/GameContainer.js
@@ -1,11 +1,26 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { Redirect } from "react-router-dom";
 import UserContext from "../contexts/UserContext";
+import FriendContext from "../contexts/FriendContext";
+import useRequest from "./../hooks/useRequest";
+import { API_URL } from "./../constants/api";
 
 const GameContainer = () => {
+  const [sendRequest, setSendRequest] = useState(false);
+  const [url, setUrl] = useState("");
   const { user } = useContext(UserContext);
-  if (user == null) {
+  const { friends } = useContext(FriendContext);
+  const [loading, status, data, error] = useRequest(
+    `${API_URL}/game/common?${url}`,
+    sendRequest,
+    "GET"
+  );
+
+  if (user == null || friends.length === 0 || friends.length > 3) {
     return <Redirect to="/" />;
+  } else if (!sendRequest) {
+    setUrl(friends.map((friend, index) => `${index ? "&" : ""}friendId=${friend.steamId}`).join(""));
+    setSendRequest(true);
   }
 
   return <div>Under construction...</div>;
---
+++ SteamMates/Controllers/GameController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SteamMates.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        [HttpGet("common")]
+        public IActionResult GetGamesInCommon([FromQuery(Name = "friendId")] string[] friendIds)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return Unauthorized("User needs to be authenticated.");
+            }
+
+            if (friendIds.Length == 0)
+            {
+                return BadRequest("No friend ID was received.");
+            }
+
+            if (friendIds.Length > 3)
+            {
+                return BadRequest("Too many friend IDs were received.");
+            }
+
+            return Ok(); // TODO: send back games
+        }
+    }
+}
