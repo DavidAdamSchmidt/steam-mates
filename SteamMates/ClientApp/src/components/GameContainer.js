55bca0300dc904c2bd9c1ff8e7a5399ba5bd5e81
David Schmidt <david.adam.schmidt@gmail.com>
2020-01-24 00:28:40 +0100

    Display commonly owned games

--- SteamMates/ClientApp/src/components/GameContainer.js
+++ SteamMates/ClientApp/src/components/GameContainer.js
@@ -3,7 +3,9 @@ import { Redirect } from "react-router-dom";
 import UserContext from "../contexts/UserContext";
 import FriendContext from "../contexts/FriendContext";
 import useRequest from "./../hooks/useRequest";
+import GameLogo from "./GameLogo";
 import { API_URL } from "./../constants/api";
+import "./../static/css/GameContainer.css";
 
 const GameContainer = () => {
   const [sendRequest, setSendRequest] = useState(false);
@@ -15,16 +17,21 @@ const GameContainer = () => {
     sendRequest,
     "GET"
   );
-  console.log(data);
 
   if (user == null || friends.length === 0 || friends.length > 3) {
     return <Redirect to="/" />;
   } else if (!sendRequest) {
     setUrl(friends.map((friend, index) => `${index ? "&" : ""}userId=${friend.steamId}`).join(""));
     setSendRequest(true);
+  } else if (loading) {
+    return <span>Loading...</span>
   }
 
-  return <div>Under construction...</div>;
+  return (
+    <div className="game-container">
+      {data && data.map(game => <GameLogo key={game.appId} game={game} />)}
+      <div>{data && `${data.length} games were found.`}</div>
+    </div>);
 };
 
 export default GameContainer;
---
+++ SteamMates/ClientApp/src/components/GameLogo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {LOGO_URL} from "../constants/steam";
+
+const GameLogo = ({ game }) => {
+  return <div className="game-logo">
+    <img src={`${LOGO_URL}/${game.appId}/${game.imgLogoUrl}.jpg`} alt="GameLogo" />
+  </div>
+};
+
+export default GameLogo;
