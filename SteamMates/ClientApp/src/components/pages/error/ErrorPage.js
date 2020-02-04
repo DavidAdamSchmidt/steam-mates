09e4bcc96e9afc2d5e0ea1ea689546abccecad4c
David Schmidt <david.adam.schmidt@gmail.com>
2020-02-04 13:27:26 +0100

    Fix being stuck on error page after login refresh

--- SteamMates/ClientApp/src/components/pages/error/ErrorPage.js
+++ SteamMates/ClientApp/src/components/pages/error/ErrorPage.js
@@ -2,9 +2,11 @@ import React, { useRef, useContext, useEffect } from "react";
 import { useHistory, Redirect } from "react-router-dom";
 import FriendContext from "../../../contexts/FriendContext";
 import "./../../../static/css/ErrorPage.css";
+import UserContext from "../../../contexts/UserContext";
 
 const ErrorPage = props => {
   const isFirstRun = useRef(true);
+  const { user } = useContext(UserContext);
   const { friends } = useContext(FriendContext);
   const history = useHistory();
 
@@ -15,6 +17,10 @@ const ErrorPage = props => {
       history.goBack();
     }
   }, [friends, history]);
+
+  if (user && friends.length === 0) {
+    return <Redirect to="/" />;
+  }
 
   const message = (((props || {}).location || {}).state || {}).message;
 
