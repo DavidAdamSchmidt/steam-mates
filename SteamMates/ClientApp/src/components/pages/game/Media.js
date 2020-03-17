d6e30e507917532afac657dad69036240eb93346
David Schmidt <david.adam.schmidt@gmail.com>
2020-03-17 13:33:06 +0100

    Display screenshots

--- SteamMates/ClientApp/src/components/pages/game/Media.js
+++ SteamMates/ClientApp/src/components/pages/game/Media.js
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
-import styled, { css } from "styled-components";
-import library_hero_default from "../../../static/images/library_hero_default.png";
-import play_video_button from "../../../static/images/play_video_button.png";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Thumbnails from "./Thumbnails";
 
 const Container = styled.div`
   width: 600px;
 `;
 
 const Video = styled.video`
-  width: 600px;
+  width: 100%;
   height: 338px;
   outline: 0;
+`;
+
+const Screenshot = styled.img`
+  width: 100%;
+  height: 338px;
+  cursor: pointer;
 `;
 
 const SelectorMenu = styled.div`
@@ -19,65 +24,49 @@ const SelectorMenu = styled.div`
   overflow-x: auto;
 `;
 
-const ThumbnailContainer = styled.div`
-  position: relative;
-  width: 116px;
-  height: 66px;
-
-  ${({ selected }) =>
-    selected
-      ? css`
-          border: 2px solid dodgerblue;
-          padding: 0;
-        `
-      : css`
-          padding: 2px;
-        `};
-`;
-
-const Thumbnail = styled.img`
-  width: 116px;
-  height: 66px;
-  cursor: pointer;
-`;
-
-const PlayButton = styled.img`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 40px;
-  height: 40px;
-  cursor: pointer;
-`;
-
 const Media = ({ data }) => {
   const [selected, setSelected] = useState(
-    (data.movies || [{ webm: {} }])[0].webm.max
+    ((data.movies || [{}])[0].webm || {}).max
   );
+  const [selectedIsVideo, setSelectedIsVideo] = useState(
+    selected !== undefined
+  );
+
+  useEffect(() => {
+    if (!selectedIsVideo) {
+      setSelected((data.screenshots || [])[0].full);
+    }
+  }, [selectedIsVideo, data.screenshots]);
 
   return (
     <Container>
       <div>
-        <Video key={selected} controls autoPlay muted>
-          <source src={selected} type="video/webm" />
-        </Video>
+        {selectedIsVideo && (
+          <Video key={selected} controls autoPlay muted>
+            <source src={selected} type="video/webm" />
+          </Video>
+        )}
+        {!selectedIsVideo && (
+          <a href={selected} target="_blank" rel="noopener noreferrer">
+            <Screenshot src={selected} alt="Screenshot" />
+          </a>
+        )}
       </div>
       <SelectorMenu>
-        {(data.movies || []).map(movie => (
-          <ThumbnailContainer
-            key={movie.id}
-            onClick={() => setSelected((movie.webm || {}).max)}
-            selected={selected === (movie.webm || {}).max}
-          >
-            <Thumbnail
-              src={movie.thumbnail}
-              alt="Thumbnail"
-              onError={e => (e.target.src = library_hero_default)}
-            />
-            <PlayButton src={play_video_button} />
-          </ThumbnailContainer>
-        ))}
+        <Thumbnails
+          thumbnails={data.movies || []}
+          areVideos={true}
+          selected={selected}
+          setSelected={setSelected}
+          setSelectedIsVideo={setSelectedIsVideo}
+        />
+        <Thumbnails
+          thumbnails={data.screenshots || []}
+          areVideos={false}
+          selected={selected}
+          setSelected={setSelected}
+          setSelectedIsVideo={setSelectedIsVideo}
+        />
       </SelectorMenu>
     </Container>
   );
