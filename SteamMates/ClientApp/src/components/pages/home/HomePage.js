f562f6a525a0060cdfa70943e764465456dec469
David Schmidt <david.adam.schmidt@gmail.com>
2020-03-28 12:38:03 +0100

    Add animation to home page

--- SteamMates/ClientApp/src/components/pages/home/HomePage.js
+++ SteamMates/ClientApp/src/components/pages/home/HomePage.js
@@ -1,9 +1,51 @@
 import React, { useContext } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import UserContext from "../../../contexts/UserContext";
 import { showError } from "../../../utils/errorUtils";
 import hero from "../../../static/images/home_page_hero.jpg";
 import { NETWORK_ERROR } from "../../../constants/request";
+
+const moveInFromLeft = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateX(-30%);
+  }
+  
+  84% {
+    transform: translateX(0.9%);
+  }
+  
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+const moveInFromRight = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateX(30%);
+  }
+  
+  84% {
+    transform: translateX(-0.9%);
+  }
+  
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+const Header = styled.div`
+  position: relative;
+  width: 100%;
+  max-width: 1050px;
+  height: 100%;
+  max-height: 61vh;
+  background: url(${hero}) no-repeat center;
+  background-size: cover;
+`;
 
 const Wrapper = styled.div`
   margin-top: -55px;
@@ -13,13 +55,31 @@ const Wrapper = styled.div`
   color: white;
 `;
 
-const Header = styled.div`
-  width: 100%;
-  max-width: 1050px;
-  height: 100%;
-  max-height: 61vh;
-  background: url(${hero}) no-repeat center;
-  background-size: cover;
+const TextBox = styled.div`
+  position: absolute;
+  top: 18%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  margin-left: 10px;
+  font-weight: bold;
+  text-transform: uppercase;
+  white-space: nowrap;
+`;
+
+const TextBoxMain = styled.span`
+  display: block;
+  animation: ${moveInFromLeft} 1.6s ease-out;
+  margin-bottom: 10px;
+  font-size: 72px;
+  letter-spacing: 20px;
+`;
+
+const TextBoxSub = styled.span`
+  display: block;
+  animation: ${moveInFromRight} 1.6s ease-out;
+  font-size: 18px;
+  letter-spacing: 5.3px;
+  color: orange;
 `;
 
 const HomePage = () => {
@@ -35,7 +95,12 @@ const HomePage = () => {
 
   return (
     <Wrapper>
-      <Header />
+      <Header>
+        <TextBox>
+          <TextBoxMain>steam mates</TextBoxMain>
+          <TextBoxSub>find the best games to play with your friends</TextBoxSub>
+        </TextBox>
+      </Header>
     </Wrapper>
   );
 };
